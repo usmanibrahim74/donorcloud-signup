4be4d33a71472a31c72c4818bfba2871f86cd040
Asad <muhammadasadbutt328@gmail.com>
2023-02-06 13:07:15 +0500

    currency

--- src/components/CurrencyInput.vue
+++ src/components/CurrencyInput.vue
@@ -1,23 +1,29 @@
 <template>
-    <input
-      ref="inputRef"
-      type="text"
-    />
-  </template>
-  
-  <script>
-  import { useCurrencyInput } from 'vue-currency-input'
-  
-  export default {
-    name: 'CurrencyInput',
-    props: {
-      modelValue: Number, // Vue 2: value
-      options: Object
-    },
-    setup(props) {
-      const { inputRef } = useCurrencyInput(props.options)
-  
-      return { inputRef }
-    }
+  <input
+    ref="inputRef"
+    type="text"
+    v-model="model"
+  />
+</template>
+
+<script>
+import { useCurrencyInput } from 'vue-currency-input'
+import { computed } from "@vue/reactivity";
+
+export default {
+  name: 'CurrencyInput',
+  props: {
+    modelValue: Number, // Vue 2: value
+    options: Object
+  },
+  setup(props, { emit }) {
+    const model = computed({
+    get: () => props.modelValue,
+    set: (value) => emit("update:modelValue", value),
+  });
+    const { inputRef } = useCurrencyInput(props.options)
+
+    return { inputRef, model }
   }
-  </script>
+}
+</script>
